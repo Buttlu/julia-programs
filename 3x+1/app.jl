d0c5463f00503bcd420921103060386580c3ea2a
a22jacka <a22jacka@student.his.se>
2024-06-20 11:29:39 +0200

    cleaned up code and gave the user more options

--- 3x+1/app.jl
+++ 3x+1/app.jl
@@ -1,22 +1,34 @@
-using Plots, GR, PlotlyJS, Printf
+using Plots, GR, Printf
 
 struct Point
     x::Int32
     y::Int32
 end
 
-print("Type number to start with: ")
-input = split(readline(), " ")
-global START_NUMBER = parse(Int32, input[size(input, 1)])
+function getStartNumber()
+    print("Type number to start with: ")
+    input = split(readline(), " ")
+    local startNumber = 0
+    try
+        startNumber = parse(Int32, input[size(input, 1)])
+        return startNumber
+    catch e
+        println("Please enter a valid number.")
+        getStartNumber()
+    end
+end
 
-print("Include the y-values for each point (y / n): ")
-input = split(readline(), " ")
-include_numbers = input[size(input, 1)]
-
-
-global steps = 1
-global points = []
-global yCoords = []
+function getShouldNumbersShow()
+    print("Include the y-values for each point (y / n): ")
+    input = split(readline(), " ")
+    local response = lowercase(input[size(input, 1)])
+    if response == "y" || response == "n"
+        return response
+    else
+        println("Please choose one of the options")
+        getShouldNumbersShow()
+    end
+end
 
 function even(x)
     return x / 2
@@ -26,10 +38,13 @@ function odd(x)
     return 3 * x + 1
 end
 
-function performCalc()
+function performCalc(START_NUMBER)
+    local steps = 1
+    local points = []
+    local yCoords = []
     push!(points, Point(steps, START_NUMBER))
     while true
-        global steps += 1
+        steps += 1
         last_point = points[size(points, 1)]
         y = last_point.y % 2 == 0 ? even(last_point.y) : odd(last_point.y)
         point = Point(steps, y)
@@ -38,24 +53,36 @@ function performCalc()
             break
         end
     end
+    # save all the y-coords to a different array to use in the scatter function later
     foreach(p -> push!(yCoords, p.y), points)
+    return points, yCoords, steps
 end
 
-performCalc()
+function drawGraph()
+    START_NUMBER = getStartNumber()
+    points, yCoords, steps = performCalc(START_NUMBER)
 
-gr()
-p = Plots.plot(scatter!(1:steps, yCoords, mc=:green, ms=5, ma=1))
-for i = 1:size(points, 1)-1
-    plot!([points[i].x, points[i+1].x], [points[i].y, points[i+1].y], linecolor=:green, linewidth=2)
-end
-if lowercase(include_numbers) == "y"
-    foreach(point -> annotate!(point.x, point.y, Plots.text("$(point.y)", :black, :right, :10)), points)
-end
-plot!(legend=:false)
-title!("3x+1 pattern, starting with $START_NUMBER")
-xlabel!("x")
-ylabel!("y")
-display(p)
 
-#readline()
-Plots.pdf("graph")
+    gr()
+    p = Plots.plot(scatter!(1:steps, yCoords, mc=:green, ms=5, ma=1))
+    # draw the lines, drawn using [x1, x2], [y1, y2]
+    for i = 1:size(points, 1)-1
+        plot!([points[i].x, points[i+1].x], [points[i].y, points[i+1].y], linecolor=:green, linewidth=2)
+    end
+    # add the numbers showing the y-level of the point
+    if lowercase(getShouldNumbersShow()) == "y"
+        foreach(point -> annotate!(point.x, point.y, Plots.text("$(point.y)", :black, :right, :10)), points)
+    end
+    plot!(legend=:false)
+    title!("3x+1 pattern, starting with $START_NUMBER, took $(size(points, 1)) steps")
+    xlabel!("x")
+    ylabel!("y")
+    display(p)
+
+
+    println("Press any key to continue...")
+    readline()
+    Plots.pdf("graph")
+end
+
+drawGraph()
